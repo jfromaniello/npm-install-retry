a9c103eb8445c0cac39625ded40c033556ba971e
José F. Romaniello <jfromaniello@gmail.com>
2013-06-29 10:38:07 -0300

    add to process.env

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = function (command, args, options, callback) {
     var runCmd = command + ' ' + args;
     console.log(('attempt ' + times).bold.green + ': ' + runCmd);
 
-    var attempt = exec(runCmd, {env: {'npm_config_color': 0}}, function (err, stdout, stderr) {
+    process.env.npm_config_color = 0;
+
+    var attempt = exec(runCmd, function (err, stdout, stderr) {
       if (stdout.match(/npm ERR\! cb\(\) never called\!/ig) || stderr.match(/npm ERR\! cb\(\) never called\!/ig)) {
         if (times >= options.attempts) {
           return callback(new Error('too many attempts'));
